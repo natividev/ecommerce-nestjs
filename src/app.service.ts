9aaaa10c4ccdd1a1246f816fcdae6a3f7c32fe77
Natividad <josenatividadcv@gmail.com>
2024-06-23 17:38:21 -0600

    feat: envio de correo electronico y docker database

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { MailService } from './mail/mail.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private mailService: MailService) {}
+
+  async getHello() {
+    const user = {
+      email: 'coderlifesv@gmail.com',
+      name: 'nativi',
+    };
+    await this.mailService.sendUserConfirmation(user, 'DCL22');
   }
 }

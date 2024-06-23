19b000db628833ecac35290a865944b917d3aaee
José Natividad Chávez <42912960+natividev@users.noreply.github.com>
2024-06-23 17:54:50 -0600

    feat: envio de correo electronico y docker database (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce')
+    .setDescription('The ecommerce API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('https://staging.yourapi.com/', 'Staging')
+    .addServer('https://production.yourapi.com/', 'Production')
+    .addTag('Endpoint')
+    .addBearerAuth()
+    .setContact(
+      'Soporte',
+      'http://mi-ecommerce.com/soporte',
+      'soporte@mi-ecommerce.com',
+    )
+    .setLicense('MIT', 'https://opensource.org/licenses/MIT')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
